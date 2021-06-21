2b2723c48b84e768da0f7a4fb5add7702392ae8c
MKCH01 <iammnsh01@gmail.com>
2021-06-21 18:19:14 +0530

    project page completed

--- src/components/Project.js
+++ src/components/Project.js
@@ -11,7 +11,7 @@ function Project(props) {
                 <div style={{paddingTop: "5rem"}}  className="education_right">
                 <h1>Live Project :-</h1>
                     <div className="graduation_div">
-                        <b>Career Finder (Using MERN-STACK)</b><br />
+                        <b>Career Finder (Clone Of Internshala, Using MERN-STACK)</b><br />
                         <b>Role Based Authentication (e.g- for user and admin seperate)</b> <br />
 
                         <b>Live Link: </b>
@@ -28,20 +28,43 @@ function Project(props) {
                         </button>
                         </a><br />
 
-                        <b>Jun 2021 - Present</b><br />
+                        <b>02 Sep 2020 - 24 Sep 2020 (Major Project)</b><br />
                     </div>
 
                     <div className="graduation_div">
-                        <b>Web Developer Trainee</b><br />
-                        <b>Unilog Content Solution Pvt. Ltd.</b><br />
-                        <b>April 2021 - jun 2021</b><br />
+                        <b>Ticket Master (Using ReactJS-Redux and Rest-API)</b><br />
+                        <b>Simple Authentication </b> <br />
+
+                        <b>Github Link: </b>
+                        <a href="https://github.com/manish221298/ticket-master" target="_blank" rel="noopener noreferrer">
+                        <button>
+                        <strong>TICKET MASTER</strong>
+                        </button>
+                        </a><br />
+
+                        <b>May 2020 - May 2020 (Major Project)</b><br />
                     </div>
 
                     <div className="graduation_div">
-                        <b>Full stack Developer Intern/ Trainee</b><br />
-                        <b>DCT Academy, Bangalore</b><br />
-                        <b>Feb 2020 - Aug 2020</b><br />
+                        <b>Time Velocity Calculator (Using ReactJS-Redux)</b><br />
+
+                        <b>Live Link: </b>
+                        <a href="https://manish-time-velocity-calculator.netlify.app/" target="_blank" rel="noopener noreferrer">
+                        <button>
+                        <strong>TIME VELOCITY CALCULATOR</strong>
+                        </button>
+                        </a><br />
+
+                        <b>Github Link: </b>
+                        <a href="https://github.com/manish221298/time_velocity_calculator" target="_blank" rel="noopener noreferrer">
+                        <button>
+                        <strong>TIME VELOCITY CALCULATOR</strong>
+                        </button>
+                        </a><br />
+
+                        <b>03 Jan 2021 - 03 Jan 2020 (Minor Project)</b><br />
                     </div>
+
                 </div>
             </div>
         </div>

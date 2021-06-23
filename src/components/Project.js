19f20576cb9a790ac41ae2ed1c3ca8c91618bbf4
MKCH01 <iammnsh01@gmail.com>
2021-06-23 13:11:43 +0530

    add project

--- src/components/Project.js
+++ src/components/Project.js
@@ -10,6 +10,7 @@ function Project(props) {
             <div className="right">
                 <div style={{paddingTop: "5rem"}}  className="education_right">
                 <h1>Live Project :-</h1>
+                <div className="scroll">
                     <div className="graduation_div">
                         <b>Career Finder (Clone Of Internshala, Using MERN-STACK)</b><br />
                         <b>Role Based Authentication (e.g- for user and admin seperate)</b> <br />
@@ -29,6 +30,7 @@ function Project(props) {
                         </a><br />
 
                         <b>02 Sep 2020 - 24 Sep 2020 (Major Project)</b><br />
+                        
                     </div>
 
                     <div className="graduation_div">
@@ -65,6 +67,66 @@ function Project(props) {
                         <b>03 Jan 2021 - 03 Jan 2020 (Minor Project)</b><br />
                     </div>
 
+                    <div className="graduation_div">
+                        <b>PRODUCT FILTER APP (Using ReactJS-Redux)</b><br />
+
+                        <b>Live Link: </b>
+                        <a href="https://manish-productlistfilter-app.herokuapp.com/" target="_blank" rel="noopener noreferrer">
+                        <button>
+                        <strong>PRODUCT FILTER APP</strong>
+                        </button>
+                        </a><br />
+
+                        <b>Github Link: </b>
+                        <a href="https://github.com/manish221298/productlist_filter" target="_blank" rel="noopener noreferrer">
+                        <button>
+                        <strong>PRODUCT FILTER APP</strong>
+                        </button>
+                        </a><br />
+
+                        <b>05 Mar 2021 - 06 Mar 2020 (Minor Project)</b><br />
+                    </div>
+
+                    <div className="graduation_div">
+                        <b>DYNAMIC FORM (Using ReactJS)</b><br />
+
+                        <b>Live Link: </b>
+                        <a href="https://manish-staticform.herokuapp.com/" target="_blank" rel="noopener noreferrer">
+                        <button>
+                        <strong>DYNAMIC FORM</strong>
+                        </button>
+                        </a><br />
+
+                        <b>Github Link: </b>
+                        <a href="https://github.com/manish221298/digiapt_assignment" target="_blank" rel="noopener noreferrer">
+                        <button>
+                        <strong>DYNAMIC FORM</strong>
+                        </button>
+                        </a><br />
+
+                        <b>17 Oct 2020 - 17 Oct 2020</b><br />
+                    </div>
+
+                    <div className="graduation_div">
+                        <b>PORTFOLIO PROJECT (Using ReactJS)</b><br />
+
+                        <b>Live Link: </b>
+                        <a href="https://manish-portfolio-app.herokuapp.com/" target="_blank" rel="noopener noreferrer">
+                        <button>
+                        <strong>PORTFOLIO PROJECT</strong>
+                        </button>
+                        </a><br />
+
+                        <b>Github Link: </b>
+                        <a href="https://github.com/manish221298/portfolio" target="_blank" rel="noopener noreferrer">
+                        <button>
+                        <strong>PORTFOLIO PROJECT</strong>
+                        </button>
+                        </a><br />
+
+                        <b>21 Jun 2021 - 22 Jun 2021</b><br />
+                    </div>
+                </div>
                 </div>
             </div>
         </div>
